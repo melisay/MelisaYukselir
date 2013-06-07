10566a137696355444f8f881c14994f5693b161d
Melisa Yukselir <melisa@fuzzycows.com>
2013-06-07 13:30:01 +0100

    style fixes

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
 //Initialize
 $(document).ready(function(){
+	//Examples
 	
-	var oSlider2 = $('#slider2');
+	
+	var oSlider2 = $('.slider2');
 	if(oSlider2.length > 0){
-		oSlider2.tinycarousel({ display: 3});
+		oSlider2.tinycarousel({ display: 4});
+	}
+	
+			
+	
+
+	
 });
 
